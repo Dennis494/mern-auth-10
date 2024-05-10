623d633971b309b9d8cfa95ee985b624f85da380
Dennis Agbo <dennisjr4557@gmail.com>
2024-05-10 23:35:16 +0100

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+//initialize dotenv
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to MongoDB');
+}) .catch((err) => {
+    console.log(err);
+});
 
 //create application
 const app = express();
