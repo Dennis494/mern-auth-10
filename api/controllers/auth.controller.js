10dcabca2d5fdd88a479fca51b8ebd546ca853fe
Dennis Agbo <dennisjr4557@gmail.com>
2024-05-11 16:17:14 +0100

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from "bcryptjs";//we use 'bycryptjs' because 'bcrypt' usually developes issues during production
+import { errorHandler } from '../utils/error.js';
+import jwt from "jsonwebtoken";
 
 //"req" is data we are getting from the client side and "res" is data we send back to the client
 export const signup = async (req, res, next) => {//we include an 'async' function
@@ -10,8 +12,24 @@ export const signup = async (req, res, next) => {//we include an 'async' functio
         await newUser.save()//save the data to the db (due to intrnet connection, 'await' is used to tell the javascript to stay in this code until it recieves the result, it helps us not to get an error while waiting for the result)
         res.status(201).json({message: 'User created successfully'});
     } catch (error) {
-        next(error);//use the middleware we just created for the erro message
+        next(error);//use the middleware we just created for the error message
     }
 
 };
 
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));//check for email
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'wrong credentials'));//check if password is correct
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);//token for extra security
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000);//1 hour
+
+        res.cookie('access_token', token, { httpOnly: true, expires: expiryDate}).status(200).json(rest)//add it inside the browser token
+    } catch (error) {
+      next(error);
+    }
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';//do not forget to always add .js at the end
+import { signin, signup } from '../controllers/auth.controller.js';//do not forget to always add .js at the end
 
 //create the router
 const router = express.Router();//using uppercase Router
 
 //create the first method
 router.post("/signup", signup)
+router.post("/signin", signin)
 
 //export the router so it can be used outside this file
 export default router;
